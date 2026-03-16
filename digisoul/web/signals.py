ba2d9c2d4882d2db1b51ff7a2a4efcb6dcb54946
shengwenmacmini <shengwen9690@163.com>
2026-03-16 16:38:47 +0800

    fix(photo): 修复在删除角色时，无法正确删除对应的图片的错误问题

--- digisoul/web/signals.py
+++ digisoul/web/signals.py
@@ -2,8 +2,19 @@ from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from web.models.Friends import Message
 from web.utils.delete_old_audio import delete_old_audio
+from web.models.Character import Character
+from web.utils.delete_old_photo import delete_old_photo
 
 
 @receiver(pre_delete, sender=Message)
 def cleanup_message_audio(sender, instance, **kwargs):
     delete_old_audio(instance.audio_message)
+
+
+@receiver(pre_delete, sender=Character)
+def cleanup_character_photo(sender, instance, **kwargs):
+    delete_old_photo(instance.photo)
+
+@receiver(pre_delete, sender=Character)
+def cleanup_character_background_photo(sender, instance, **kwargs):
+    delete_old_photo(instance.background_photo)
