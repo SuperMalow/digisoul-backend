5e45c0e790d7ff086003cd870dd9e327f6cab248
shengwenmacmini <shengwen9690@163.com>
2026-03-17 03:06:22 +0800

    fix(file): 针对角色音色模型删除后，一并将音频文件删去

--- digisoul/web/signals.py
+++ digisoul/web/signals.py
@@ -2,14 +2,13 @@ from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from web.models.Friends import Message
 from web.utils.delete_old_audio import delete_old_audio
-from web.models.Character import Character
+from web.models.Character import Character, CharacterVoice
 from web.utils.delete_old_photo import delete_old_photo
 
 
 @receiver(pre_delete, sender=Message)
 def cleanup_message_audio(sender, instance, **kwargs):
     delete_old_audio(instance.audio_message)
-
 
 @receiver(pre_delete, sender=Character)
 def cleanup_character_photo(sender, instance, **kwargs):
@@ -18,3 +17,7 @@ def cleanup_character_photo(sender, instance, **kwargs):
 @receiver(pre_delete, sender=Character)
 def cleanup_character_background_photo(sender, instance, **kwargs):
     delete_old_photo(instance.background_photo)
+
+@receiver(pre_delete, sender=CharacterVoice)
+def cleanup_character_voice(sender, instance, **kwargs):
+    delete_old_audio(instance.preview_voice)
