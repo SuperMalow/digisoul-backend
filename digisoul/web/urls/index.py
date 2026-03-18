090ffedf133b9be912cc7c6a0b306953b8ab69a0
shengwenmacmini <shengwen9690@163.com>
2026-03-19 01:44:08 +0800

    style(urls): 针对所有urls都添加 api 前缀

--- digisoul/web/urls/index.py
+++ digisoul/web/urls/index.py
@@ -2,15 +2,15 @@ from django.urls import path, include, re_path
 from web.views.index import index
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('user/', include('web.urls.user')),
-    path('token/', include('web.urls.token')),
-    path('character/', include('web.urls.character')),
-    path('friends/', include('web.urls.friends')),
-    path('character/settings/', include('web.urls.characterSettings')),
-    path('character/voice/', include('web.urls.characterVoice')),
-    path('', include('web.urls.tti')),
+    # path('', index, name='index'),
+    path('api/user/', include('web.urls.user')),
+    path('api/token/', include('web.urls.token')),
+    path('api/character/', include('web.urls.character')),
+    path('api/friends/', include('web.urls.friends')),
+    path('api/character/settings/', include('web.urls.characterSettings')),
+    path('api/character/voice/', include('web.urls.characterVoice')),
+    path('api/tti/', include('web.urls.tti')),
 
-    # 匹配所有非 media/、static/ 和 assets/ 的请求，并返回 index.html
-    re_path(r'^(?!media/|static/|assets/).*$', index)
+    # # 匹配所有非 media/、static/ 和 assets/ 的请求，并返回 index.html
+    # re_path(r'^(?!media/|static/|assets/).*$', index)
 ]
--- digisoul/web/urls/tti.py
+++ digisoul/web/urls/tti.py
@@ -9,14 +9,14 @@ from web.views.tti.tti import (
 
 urlpatterns = [
     # 角色性格优化
-    path('tti/character/profile/optimization/', CharacterProfileOptimizationView.as_view(), name='character_profile_optimization'),
+    path('character/profile/optimization/', CharacterProfileOptimizationView.as_view(), name='character_profile_optimization'),
     # 根据角色性格生成图像
-    path('tti/generate/image/prompt/', GenerateImagePromptView.as_view(), name='generate_image_prompt'),
+    path('generate/image/prompt/', GenerateImagePromptView.as_view(), name='generate_image_prompt'),
 
     # 根据提示词生成图像
-    path('tti/generate/image/', GenerateImageView.as_view(), name='generate_image'),
+    path('generate/image/', GenerateImageView.as_view(), name='generate_image'),
     # 根据 task_id 查询生成状态
-    path('tti/generate/image/task/', GenerateImageTaskStatusView.as_view(), name='generate_image_task_status'),
+    path('generate/image/task/', GenerateImageTaskStatusView.as_view(), name='generate_image_task_status'),
     # 根据 task_id 获取生成图片文件（后端代理下载，规避浏览器跨域限制）
-    path('tti/generate/image/task/file/', GenerateImageTaskFileView.as_view(), name='generate_image_task_file'),
+    path('generate/image/task/file/', GenerateImageTaskFileView.as_view(), name='generate_image_task_file'),
 ]
